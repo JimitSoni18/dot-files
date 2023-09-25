382bf822446f0becd2625cf3f885ac8cbc8d18a9
Jimit Dholakia <jimitsoni18@gmail.com>
2023-09-25 23:53:59 +0530

    wezterm should work and added JetBrainsMono

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+local config = {}
+config.font = wezterm.font 'JetBrains Mono'
+
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
+
+config.color_scheme = 'One Dark (Gogh)'
+
+return config
